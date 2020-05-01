524c300055f9408862222fd55fbf51603d8a389d
Pavol Pidanic <paulp@hotmail.sk>
2020-05-01 11:14:00 +0200

    Main menu with scrollable screens

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { Provider as PaperProvider, Appbar, ToolbarContent } from 'react-native-paper';
+import React, { useState, useRef, useEffect } from 'react';
+import { Provider as PaperProvider, Appbar, Button } from 'react-native-paper';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import Swiper from 'react-native-swiper';
 import ProductScreen from './ProductScreen';
 import ShopScreen from './ShopScreen';
 
+const styles = StyleSheet.create({
+  buttonColorActive: {
+    borderBottomColor: 'white',
+    borderBottomWidth: 4,
+    borderTopStartRadius: 0,
+    borderTopEndRadius: 0,
+    borderBottomStartRadius: 0,
+    borderBottomEndRadius: 0,
+    flex: 1,
+  },
+  buttonColor: {
+    // borderBottomColor: 'white',
+    // borderBottomWidth: 4,
+    borderTopStartRadius: 0,
+    borderTopEndRadius: 0,
+    borderBottomStartRadius: 0,
+    borderBottomEndRadius: 0,
+    flex: 1,
+  },
+  alignmentButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    padding: 0,
+    marginBottom: 0,
+  }
+});
+
+
 export default function App() {
   const [screenIndex, setSceenIndex] = useState(0);
 
+  const swiperRef = useRef(null);
+
+  const onMenuClick = (index) => {
+    swiperRef.current.scrollBy((screenIndex - index) * -1);
+  };
+
   return (
     <PaperProvider>
-      <Appbar.Header>
-        <Appbar.Content
-          title="Heureka"
-        />
-      </Appbar.Header>
-      <Swiper showsPagination={false} loop={true} bounces={true} onIndexChanged={index => setSceenIndex({ index })} >
+        <Appbar.Header>
+          <Appbar.Content
+            title="Heureka"
+            subtitle="Pre potraviny"
+          />
+        </Appbar.Header>
+      <View style={styles.alignmentButtons}>
+        <Button compact uppercase={false} icon="magnify" mode="contained" style={screenIndex === 0 ? styles.buttonColorActive : styles.buttonColor} onPress={() => onMenuClick(0)}>Najlacnejší produkt</Button>
+        <Button compact uppercase={false} icon="cart-outline" mode="contained" style={screenIndex === 1 ? styles.buttonColorActive : styles.buttonColor} onPress={() => onMenuClick(1)}>Môj nákup</Button>
+      </View>
+      <Swiper ref={swiperRef} showsPagination={false} loop={true} bounces={true} onIndexChanged={index => setSceenIndex(index)} >
         <ProductScreen />
         <ShopScreen />
       </Swiper>

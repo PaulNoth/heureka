6668f2de14c09fd40bdb15537680ccf4b9976708
Pavol Pidanic <paulp@hotmail.sk>
2020-05-02 10:56:43 +0200

    eslint configuration

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
-      <Counter/>
+      <Counter />
     </PaperProvider>
   );
 }
--- Counter.js
+++ Counter.js
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, Badge } from 'react-native-paper';
 
 export default function Counter() {
-    const [count, setCount] = useState(0);
-    return (
-        <View style={styles.container}>
-            <Badge>{count}</Badge>
-            <Button mode="contained" onPress={() => setCount(count + 1)}>
-                Increment counter
-            </Button>
-        </View>
-    );
+  const [count, setCount] = useState(0);
+  return (
+    <View style={styles.container}>
+      <Badge>{count}</Badge>
+      <Button mode="contained" onPress={() => setCount(count + 1)}>
+        Increment counter
+      </Button>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({

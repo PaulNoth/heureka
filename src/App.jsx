cf630fa12d0f8f1afd9ebb8795955c6b25012889
Pavol Pidanic <paulp@hotmail.sk>
2020-05-02 17:58:15 +0200

    eslint fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,81 @@
 import React, { useState, useRef } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { Provider as PaperProvider, Appbar, Button } from 'react-native-paper';
-import {
-  StyleSheet, View,
-} from 'react-native';
 import Swiper from 'react-native-swiper';
+
 import ProductScreen from './ProductScreen';
 import ShopScreen from './ShopScreen';
 
 const styles = StyleSheet.create({
-  buttonColorActive: {
-    borderBottomColor: 'white',
-    borderBottomWidth: 4,
-    borderTopStartRadius: 0,
-    borderTopEndRadius: 0,
-    borderBottomStartRadius: 0,
-    borderBottomEndRadius: 0,
-    flex: 1,
-  },
-  buttonColor: {
-    // borderBottomColor: 'white',
-    // borderBottomWidth: 4,
-    borderTopStartRadius: 0,
-    borderTopEndRadius: 0,
-    borderBottomStartRadius: 0,
-    borderBottomEndRadius: 0,
-    flex: 1,
-  },
-  alignmentButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    padding: 0,
-    marginBottom: 0,
-  },
+    buttonColorActive: {
+        borderBottomColor: 'white',
+        borderBottomWidth: 4,
+        borderTopStartRadius: 0,
+        borderTopEndRadius: 0,
+        borderBottomStartRadius: 0,
+        borderBottomEndRadius: 0,
+        flex: 1,
+    },
+    buttonColor: {
+        // borderBottomColor: 'white',
+        // borderBottomWidth: 4,
+        borderTopStartRadius: 0,
+        borderTopEndRadius: 0,
+        borderBottomStartRadius: 0,
+        borderBottomEndRadius: 0,
+        flex: 1,
+    },
+    alignmentButtons: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        padding: 0,
+        marginBottom: 0,
+    },
 });
 
 export default function App() {
-  const [screenIndex, setSceenIndex] = useState(0);
+    const [screenIndex, setSceenIndex] = useState(0);
 
-  const swiperRef = useRef(null);
+    const swiperRef = useRef(null);
 
-  const onMenuClick = (index) => {
-    swiperRef.current.scrollBy((screenIndex - index) * -1);
-  };
+    const onMenuClick = (index) => {
+        swiperRef.current.scrollBy((screenIndex - index) * -1);
+    };
 
-  return (
-    <PaperProvider>
-      <Appbar.Header>
-        <Appbar.Content
-          title="Heureka"
-          subtitle="Pre potraviny"
-        />
-      </Appbar.Header>
-      <View style={styles.alignmentButtons}>
-        <Button compact uppercase={false} icon="magnify" mode="contained" style={screenIndex === 0 ? styles.buttonColorActive : styles.buttonColor} onPress={() => onMenuClick(0)}>Najlacnejší produkt</Button>
-        <Button compact uppercase={false} icon="cart-outline" mode="contained" style={screenIndex === 1 ? styles.buttonColorActive : styles.buttonColor} onPress={() => onMenuClick(1)}>Môj nákup</Button>
-      </View>
-      <Swiper
-        ref={swiperRef}
-        showsPagination={false}
-        loop
-        bounces
-        onIndexChanged={(index) => setSceenIndex(index)}
-      >
-        <ProductScreen />
-        <ShopScreen />
-      </Swiper>
-    </PaperProvider>
-  );
+    return (
+        <PaperProvider>
+            <Appbar.Header>
+                <Appbar.Content title="Heureka" subtitle="Pre potraviny" />
+            </Appbar.Header>
+            <View style={styles.alignmentButtons}>
+                <Button
+                    compact
+                    uppercase={false}
+                    icon="magnify"
+                    mode="contained"
+                    style={screenIndex === 0 ? styles.buttonColorActive : styles.buttonColor}
+                    onPress={() => onMenuClick(0)}>
+                    Najlacnejší produkt
+                </Button>
+                <Button
+                    compact
+                    uppercase={false}
+                    icon="cart-outline"
+                    mode="contained"
+                    style={screenIndex === 1 ? styles.buttonColorActive : styles.buttonColor}
+                    onPress={() => onMenuClick(1)}>
+                    Môj nákup
+                </Button>
+            </View>
+            <Swiper
+                ref={swiperRef}
+                showsPagination={false}
+                loop
+                bounces
+                onIndexChanged={(index) => setSceenIndex(index)}>
+                <ProductScreen />
+                <ShopScreen />
+            </Swiper>
+        </PaperProvider>
+    );
 }

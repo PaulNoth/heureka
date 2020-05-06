50cdae783f5b7570cf711c807e3c671337868676
Pavol Pidanic <paulp@hotmail.sk>
2020-05-06 20:41:48 +0200

    Text input with search button

--- src/ProductScreen.jsx
+++ src/ProductScreen.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TextInput, IconButton, Colors, Button } from 'react-native-paper';
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#ececec',
+        flex: 1,
+    },
+    container2: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    textInput: {
+        flex: 3,
     },
 });
 
 export default function ShopScreen() {
+    const [text, setText] = useState(null);
     return (
         <View style={styles.container}>
-            <Text>Najlacnejsi produkt</Text>
+            <View style={styles.container2}>
+                <TextInput
+                    style={styles.textInput}
+                    mode="flat"
+                    placeholder="Hľadaj najlacnejší produkt"
+                    value={text}
+                    onChangeText={(t) => setText(t)}
+                />
+                <IconButton
+                    icon="magnify"
+                    color={Colors.purple500}
+                    size={40}
+                    onPress={() => console.log('Pressed')}
+                />
+            </View>
         </View>
     );
 }

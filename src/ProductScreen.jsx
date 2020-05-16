481ae1b8709820d35914ddcaf8a83a504397d2e7
Pavol Pidanic <paulp@hotmail.sk>
2020-05-16 20:25:07 +0200

    show table

--- src/ProductScreen.jsx
+++ src/ProductScreen.jsx
@@ -1,7 +1,10 @@
+import _uniq from 'lodash/uniq';
 import React, { useState } from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import Autocomplete from 'react-native-autocomplete-input';
-import { TextInput, IconButton, DefaultTheme, Divider } from 'react-native-paper';
+import { TextInput, IconButton, DefaultTheme, Divider, DataTable, Title } from 'react-native-paper';
+
+import heurekaData from '../data/heureka.json';
 
 const styles = StyleSheet.create({
     container: {
@@ -25,20 +28,38 @@ const styles = StyleSheet.create({
     },
 });
 
-const testData = ['mineralka', 'jogurt', 'mlieko'];
+const productGroups = _uniq(heurekaData.map((hd) => hd.group));
 
 export default function ShopScreen() {
     const [text, setText] = useState(null);
     const [hidden, setHidden] = useState(true);
+    const [products, setProducts] = useState(productGroups);
+    const [selectedProduct, setSelectedProduct] = useState(null);
 
     const onTextType = (t) => {
-        if(t.length > 2) {
+        if (t.length > 2) {
+            const filteredProducts = products.filter((prodName) => prodName.startsWith(text));
             setHidden(false);
+            setProducts(filteredProducts);
         } else {
+            setProducts(productGroups);
+            // setSelectedProduct(null);
             setHidden(true);
         }
         setText(t);
-    }
+    };
+
+    const selectProduct = (item) => {
+        setSelectedProduct(item);
+        setHidden(true);
+    };
+
+    const getCheapestProducts = () => {
+        const productList = heurekaData.filter((hd) => hd.group === selectedProduct);
+        const cheapestProducts = productList.sort((a, b) => a.price - b.price);
+
+        return cheapestProducts;
+    };
 
     return (
         <View style={styles.container}>
@@ -46,10 +67,10 @@ export default function ShopScreen() {
                 <View style={{ flex: 1 }}>
                     <Autocomplete
                         hideResults={hidden}
-                        data={testData}
+                        data={products}
                         containerStyle={styles.autocompleteContainer}
                         renderItem={({ item, i }) => (
-                            <TouchableOpacity key={i} onPress={() => console.log({ query: item })}>
+                            <TouchableOpacity key={i} onPress={() => selectProduct(item)}>
                                 <Text>{item}</Text>
                             </TouchableOpacity>
                         )}
@@ -72,6 +93,31 @@ export default function ShopScreen() {
                     animated
                     onPress={() => console.log('Pressed')}
                 />
+            </View>
+            <View>
+                {selectedProduct ? (
+                    <>
+                        <Title>{selectedProduct}</Title>
+                        <DataTable>
+                            <DataTable.Header>
+                                <DataTable.Title>Obchod</DataTable.Title>
+                                <DataTable.Title>Názov</DataTable.Title>
+                                <DataTable.Title numeric>Cena</DataTable.Title>
+                            </DataTable.Header>
+                            {getCheapestProducts()
+                                .slice(0, 5)
+                                .map((prod, index) => (
+                                    <DataTable.Row key={index}>
+                                        <DataTable.Cell>{prod.merchant}</DataTable.Cell>
+                                        <DataTable.Cell>{prod.name}</DataTable.Cell>
+                                        <DataTable.Cell numeric>{`${(prod.price / 100).toFixed(
+                                            2
+                                        )} €`}</DataTable.Cell>
+                                    </DataTable.Row>
+                                ))}
+                        </DataTable>
+                    </>
+                ) : null}
             </View>
         </View>
     );
